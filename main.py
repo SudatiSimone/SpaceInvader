f63ad453adc31c6a0d5b4342fb0d3dc8d1c8ee02
SudatiSimone <39488019+SudatiSimone@users.noreply.github.com>
2020-05-01 14:31:54 +0100

    update

---
+++ main.py
@@ -0,0 +1,37 @@
+# "Icon made by Pixel perfect from www.flaticon.com"
+
+import pygame
+
+# Intialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Tile and icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('transportation.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+# Game Loop
+running = True
+while running:
+
+    # RGB - Red, Green, Blue
+    screen.fill((0, 150, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    player()
+    pygame.display.update()
